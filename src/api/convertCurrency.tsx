1565b2a35d2476070955a8a2eed31be15d39e420
Paul <paulpastor122297@gmail.com>
2025-02-24 20:33:45 +0800

    added additional env

--- src/api/convertCurrency.tsx
+++ src/api/convertCurrency.tsx
@@ -1,11 +1,12 @@
 type formType = FormDataEntryValue | null;
 
 export const convertCurrency = async(from : formType, to : formType, amount: formType) => {
-   try {
-        const response = await fetch(`http://localhost:3000/api/convert?from=${from}&to=${to}&amount=${amount}`)
-        const data = response.json()
-        return data
-   } catch(error) {
-        console.log(error)
-   }
+
+     try {
+          const response = await fetch(`${import.meta.env.VITE_PROXY_BASE_URL}/convert?from=${from}&to=${to}&amount=${amount}`)
+          const data = await response.json()
+          return data
+     } catch(error) {
+          console.log(error)
+     }
 }
